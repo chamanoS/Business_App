0507c2832ae51642840e67b0c80441690b8980f2
Chamano Ramashia <chamano.sr@gmail.com>
2022-01-20 14:52:52 +0200

    code refactoring, adding global styles

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,5 @@
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Profiles from "./pages/profiles/Profile";
-import Write from "./pages/write/Write";
+import { Navbar } from "./components"
+import { Home, Login, Register, Profile, Write } from "./pages";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "./context/Context";
@@ -18,7 +14,7 @@ function App() {
         <Route path="/register" element={user ? <Home /> : <Register />} />
         <Route path="/login" element={user ? <Home /> : <Login />} />
         <Route path="/write" element={user ? <Write /> : <Login />} />
-        <Route path="/profile" element={user ? <Profiles /> : <Login />} />
+        <Route path="/profile" element={user ? <Profile /> : <Login />} />
       </Routes>
     </Router>
   );
